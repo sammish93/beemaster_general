2a5e8f402f177a31a6f5d42fea7846d1993c58b6
Sammish <samd93@hotmail.co.uk>
2024-02-07 14:43:41 +0100

    added additional comments

--- components/layouts/login.tsx
+++ components/layouts/login.tsx
@@ -5,6 +5,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
 
+// This nav stack only allows a user to access the login page until authentication.
 export const LoginScreen = () => (
   <Stack.Navigator>
     <Stack.Screen
