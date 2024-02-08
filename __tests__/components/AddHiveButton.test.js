2326956da788f259faa43470d742cd808ae6dbcc
stian96 <stiankru@hiof.no>
2024-02-08 11:34:59 +0100

    created simple test for AddHiveButton

---
+++ __tests__/components/AddHiveButton.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import AddHiveButton from './AddHiveButton';
+
+describe('AddHiveButton', () => {
+    it('calls onAddHivePress when button is pressed', () => {
+        const onAddHivePressMock = jest.fn();
+        const { getByText } = render(<AddHiveButton onAddHivePress={onAddHivePressMock}/>);
+
+        fireEvent.press(getByText('Add New Hive'));
+        expect(onAddHivePressMock).toHaveBeenCalled();
+    })
+})
