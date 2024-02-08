1aacb46135e1345550b4365e11c72d7142525c51
stian96 <stiankru@hiof.no>
2024-02-08 11:57:55 +0100

    deleted snapshot test

--- __tests__/components/AddHiveButton.test.js
+++ __tests__/components/AddHiveButton.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
-
+import AddHiveButton from '../../components/AddHiveButton';
 
 describe('AddHiveButton', () => {
     it('calls onAddHivePress when button is pressed', () => {
