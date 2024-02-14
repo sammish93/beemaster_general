0b95db7617fdc28943a3fd9ef29591580d095d41
LorenaHolmeide <89775879+LorenaHolmeide@users.noreply.github.com>
2024-02-14 15:30:55 +0100

    update gdprDialog

--- components/modals/DialogGDPR.tsx
+++ components/modals/DialogGDPR.tsx
@@ -41,11 +41,11 @@ const MobileModal = ({ hideDialog }: DialogGDPRProps) => {
 
     return (
         <Portal>
-            <Dialog style={dynamicStyles.dialogStyleMobile} visible={true} onDismiss={hideDialog}>
+            <Dialog style={dynamicStyles.dialogStyleMobile} visible={true} onDismiss={hideDialog}  >
                 <Dialog.Icon icon="alert" />
                 <Dialog.Title style={dynamicStyles.titleDialogGDPR} >Welcome to the Beemaster General App</Dialog.Title>
                 <Dialog.Content >
-                    <Text style={theme.fonts.titleMedium}>GDPR Information and Terms of Use</Text>
+                    <Text style={theme.fonts.titleMedium} accessibilityRole="header">GDPR Information and Terms of Use</Text>
                 </Dialog.Content>
                 <Dialog.ScrollArea >
                     <ScrollView contentContainerStyle={dynamicStyles.scrollViewContent}>
@@ -60,8 +60,8 @@ const MobileModal = ({ hideDialog }: DialogGDPRProps) => {
                     </ScrollView>
                 </Dialog.ScrollArea>
                 <Dialog.Actions>
-                    <Button onPress={hideDialog}>Cancel</Button>
-                    <Button onPress={() => console.log('Ok pressed')}>Ok</Button>
+                    <Button onPress={hideDialog} accessibilityLabel="Cancel the modal" accessibilityHint="Close if disagree to terms of use">Cancel</Button>
+                    <Button onPress={() => console.log('Ok pressed')} accessibilityLabel="Ok to terms of use" accessibilityHint="Agree to terms of use">Ok</Button>
                 </Dialog.Actions>
             </Dialog>
         </Portal >
@@ -87,7 +87,7 @@ const WebModal = ({ hideDialog }: DialogGDPRProps) => {
                 <Dialog.Icon icon="alert" />
                 <Dialog.Title style={dynamicStyles.titleDialogGDPR} >Welcome to the Beemaster General App</Dialog.Title>
                 <Dialog.Content>
-                    <Text style={theme.fonts.titleMedium}>GDPR Information and Terms of Use</Text>
+                    <Text style={theme.fonts.titleMedium} accessibilityRole="header">GDPR Information and Terms of Use</Text>
                 </Dialog.Content>
                 <Dialog.ScrollArea >
                     <ScrollView contentContainerStyle={{ paddingHorizontal: 24, paddingTop: 16 }}>
@@ -96,14 +96,14 @@ const WebModal = ({ hideDialog }: DialogGDPRProps) => {
                         <Text style={dynamicStyles.listItem} > • Legal basis for the processing:....</Text>
                         <Text style={dynamicStyles.listItem} > • Third parties who may receive personal data: such as subcontractors, cloud services, marketing partners, etc.</Text>
                         <Text style={dynamicStyles.listItem} > • Storage period: Describe how long the personal data will be stored.</Text>
-                        <Text style={dynamicStyles.listItem} > • The rights of the data subject: Inform users about their rights under GDPR, such as the right to request access to, correction or deletion of the personal data stored about them, the right to object to processing, and the right to data portability.</Text>
+                        <Text style={dynamicStyles.listItem} > • The rights of rthe data subject: Inform users about their rights under GDPR, such as the right to request access to, correction or deletion of the personal data stored about them, the right to object to processing, and the right to data portability.</Text>
                         <Text style={dynamicStyles.listItem} > • The right to withdraw consent:....</Text>
                         <Text style={dynamicStyles.listItem} > • The right to lodge a complaint with a supervisory authority: Provide information on how and to whom users can complain if they believe the processing of their personal data violates GDPR.</Text >
                     </ScrollView>
                 </Dialog.ScrollArea>
                 <Dialog.Actions>
-                    <Button onPress={hideDialog}>Cancel</Button>
-                    <Button onPress={() => console.log('Ok pressed')}>Ok</Button>
+                    <Button onPress={hideDialog} accessibilityLabel="Cancel the modal" accessibilityHint="Close if disagree to terms of use">Cancel</Button>
+                    <Button onPress={() => console.log('Ok pressed')} accessibilityLabel="Ok to terms of use" accessibilityHint="Agree to terms of use">Ok</Button>
                 </Dialog.Actions>
             </Dialog>
         </Portal>
