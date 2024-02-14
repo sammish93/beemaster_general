40fad6c5eea01515623dda641d11c86c8210e12f
LorenaHolmeide <89775879+LorenaHolmeide@users.noreply.github.com>
2024-02-14 12:40:29 +0100

    Update DialogGDPR.tsx

--- components/modals/DialogGDPR.tsx
+++ components/modals/DialogGDPR.tsx
@@ -1,13 +1,23 @@
 import * as React from 'react';
 import { Button, Dialog, Portal, Text } from 'react-native-paper';
-import { ScrollView, View, Platform } from 'react-native';
+import { ScrollView, Platform } from 'react-native';
 import getStyles from '@/assets/styles';
 import { useTheme } from 'react-native-paper';
+
 
 interface DialogGDPRProps {
     hideDialog: () => void;
 
 }
+
+
+/**
+ * `DialogGDPR` component displays a GDPR consent dialog tailored to the platform.
+ * It renders `MobileModal` for Android and iOS platforms, otherwise `WebModal`.
+ * 
+ * @param hideDialog - A function to close the dialog.
+ */
+
 const DialogGDPR = ({ hideDialog }: DialogGDPRProps) => {
 
     if (Platform.OS === "android" || Platform.OS === "ios") {
@@ -18,6 +28,13 @@ const DialogGDPR = ({ hideDialog }: DialogGDPRProps) => {
 
 };
 
+/**
+ * `MobileModal` component renders the GDPR dialog specifically designed for mobile platforms.
+ * It utilizes a `Portal` to render the dialog above other content and includes GDPR information.
+ * 
+ * @param hideDialog - A function to close the dialog.
+ */
+
 const MobileModal = ({ hideDialog }: DialogGDPRProps) => {
     const dynamicStyles = getStyles();
     const theme = useTheme();
@@ -27,13 +44,11 @@ const MobileModal = ({ hideDialog }: DialogGDPRProps) => {
             <Dialog style={dynamicStyles.dialogStyleMobile} visible={true} onDismiss={hideDialog}>
                 <Dialog.Icon icon="alert" />
                 <Dialog.Title style={dynamicStyles.titleDialogGDPR} >Welcome to the Beemaster General App</Dialog.Title>
-
                 <Dialog.Content >
                     <Text style={theme.fonts.titleMedium}>GDPR Information and Terms of Use</Text>
                 </Dialog.Content>
                 <Dialog.ScrollArea >
                     <ScrollView contentContainerStyle={dynamicStyles.scrollViewContent}>
-
                         <Text style={dynamicStyles.listItem} > • Data controller: Group/HiØ</Text>
                         <Text style={dynamicStyles.listItem} > • The purpose of the processing of personal data: Clearly explain why you collect personal data, for example to improve services, customize the user experience, or for marketing purposes. </Text>
                         <Text style={dynamicStyles.listItem} > • Legal basis for the processing:....</Text>
@@ -42,11 +57,8 @@ const MobileModal = ({ hideDialog }: DialogGDPRProps) => {
                         <Text style={dynamicStyles.listItem} > • The rights of the data subject: Inform users about their rights under GDPR, such as the right to request access to, correction or deletion of the personal data stored about them, the right to object to processing, and the right to data portability.</Text>
                         <Text style={dynamicStyles.listItem} > • The right to withdraw consent:....</Text>
                         <Text style={dynamicStyles.listItem} > • The right to lodge a complaint with a supervisory authority: Provide information on how and to whom users can complain if they believe the processing of their personal data violates GDPR.</Text >
-
                     </ScrollView>
                 </Dialog.ScrollArea>
-
-
                 <Dialog.Actions>
                     <Button onPress={hideDialog}>Cancel</Button>
                     <Button onPress={() => console.log('Ok pressed')}>Ok</Button>
@@ -55,6 +67,15 @@ const MobileModal = ({ hideDialog }: DialogGDPRProps) => {
         </Portal >
     );
 };
+
+
+
+/**
+ * `WebModal` component renders the GDPR dialog specifically designed for web platforms.
+ * Similar to `MobileModal`, it presents GDPR information and terms of use within a dialog.
+ * 
+ * @param hideDialog - A function to close the dialog.
+ */
 
 const WebModal = ({ hideDialog }: DialogGDPRProps) => {
     const dynamicStyles = getStyles();
@@ -78,24 +99,16 @@ const WebModal = ({ hideDialog }: DialogGDPRProps) => {
                         <Text style={dynamicStyles.listItem} > • The rights of the data subject: Inform users about their rights under GDPR, such as the right to request access to, correction or deletion of the personal data stored about them, the right to object to processing, and the right to data portability.</Text>
                         <Text style={dynamicStyles.listItem} > • The right to withdraw consent:....</Text>
                         <Text style={dynamicStyles.listItem} > • The right to lodge a complaint with a supervisory authority: Provide information on how and to whom users can complain if they believe the processing of their personal data violates GDPR.</Text >
-
                     </ScrollView>
                 </Dialog.ScrollArea>
-
                 <Dialog.Actions>
                     <Button onPress={hideDialog}>Cancel</Button>
                     <Button onPress={() => console.log('Ok pressed')}>Ok</Button>
                 </Dialog.Actions>
             </Dialog>
-
         </Portal>
     );
 };
 
-
-
-
-
 export default DialogGDPR;
 
-/*                    */
