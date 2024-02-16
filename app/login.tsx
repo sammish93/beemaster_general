95d0f09d7fe6bfe7ed25cec5df21c6e4dac0254c
Ashti Mawloudi <111912492+a5h71@users.noreply.github.com>
2024-02-16 13:40:55 +0100

    Login page

--- app/login.tsx
+++ app/login.tsx
@@ -1,8 +1,8 @@
 import { useNavigation } from "expo-router";
-import { View } from "react-native";
+import { View, ScrollView, Pressable } from "react-native";
 import { observer, MobXProviderContext } from "mobx-react";
-import { useContext } from "react";
-import { Button, useTheme, Text } from "react-native-paper";
+import { useState, useContext } from "react";
+import { Button, useTheme, Text, TextInput } from "react-native-paper";
 import styles from "@/assets/styles";
 import { SafeAreaView } from "react-native-safe-area-context";
 import StatusBarCustom from "@/components/StatusBarCustom";
@@ -11,25 +11,88 @@ const LoginScreen = () => {
   const theme = useTheme();
   const navigation = useNavigation();
   const { userViewModel } = useContext(MobXProviderContext);
-  const { exampleViewModel } = useContext(MobXProviderContext);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const handleGoogleSignIn = () => {
+    userViewModel.signInWithGoogle();
+  };
+
+  const handleEmailSignIn = () => {
+    if (isSignUp) {
+      
+      userViewModel.signUpWithEmail(email, password);
+    } else {
+     
+      userViewModel.signInWithEmail(email, password);
+    }
+  };
+
+  const handleAnonymousSignIn = () => {
+    userViewModel.signInAnonymously();
+  };
 
   return (
     <SafeAreaView style={styles(theme).container}>
       <StatusBarCustom />
-      <View style={styles(theme).main}>
-        <Text style={theme.fonts.displayLarge}>Login Display</Text>
-        <Text style={theme.fonts.titleLarge}>Login Title</Text>
-        <Text style={theme.fonts.bodyLarge}>Login Body</Text>
-        <Text style={theme.fonts.labelLarge}>Login Label</Text>
-        <Button
-          mode="contained"
-          onPress={() => {
-            userViewModel.setUserId("exampleId");
-          }}
-        >
-          Navigate to Home Screen
-        </Button>
-      </View>
+      <ScrollView contentContainerStyle={styles(theme).main}>
+        <Text style={{ textAlign: 'center', padding: 5, marginBottom: 5}}>{isSignUp ? 'Sign Up' : 'Login'}</Text>
+        <TextInput
+          label="Email"
+          placeholder="Enter your email"
+          value={email}
+          onChangeText={setEmail}
+          style={{
+            borderWidth: 1, 
+            borderColor: 'gray', 
+            padding: 5, 
+            borderRadius: 5,
+            marginBottom: 5 
+          }}  
+        />
+        <TextInput
+          label="Password"
+          placeholder="Enter your password"
+          value={password}
+          secureTextEntry
+          onChangeText={setPassword}
+          style={{
+            borderWidth: 1, 
+            borderColor: 'gray', 
+            padding: 5, 
+            borderRadius: 5,
+            marginBottom: 5 
+          }}  
+          
+        />
+        {isSignUp ? (
+          <>
+            
+            <Button icon="email" mode="contained"  onPress={handleEmailSignIn}>
+              Sign Up 
+            </Button>
+          </>
+        ) : (
+          <>
+            <Button icon="email" mode="contained" style = {{marginBottom: 10}} onPress={handleEmailSignIn}>
+              Login with Email
+            </Button>
+            <Text style={{ textAlign: 'center'}}>{"OR"}</Text>
+            <Button icon="google" mode="contained" style = {{marginBottom: 10}} onPress={handleGoogleSignIn}>
+              Login with Google
+            </Button>
+            <Button icon="incognito" mode="contained" style = {{marginBottom: 10}} onPress={handleAnonymousSignIn}>
+              Login Anonymously
+            </Button>
+          </>
+        )}
+        <Pressable onPress={() => setIsSignUp(!isSignUp)}>
+          <Text style={{ textDecorationLine: 'underline', color: theme.colors.primary, textAlign: 'center' }}>
+            {isSignUp ? 'Already have an account? Sign in' : "Don't have an account? Sign up"}
+          </Text>
+        </Pressable>
+      </ScrollView>
     </SafeAreaView>
   );
 };
