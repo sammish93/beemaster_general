696fcbd858c3c1dc582b99fe3e612b3b4cc15b73
stian96 <stiankru@hiof.no>
2024-02-21 18:55:09 +0100

    wrote more tests for HiveInfo.

--- __tests__/components/HiveInfo.test.js
+++ __tests__/components/HiveInfo.test.js
@@ -7,9 +7,14 @@ const mockHiveItem = {
 }
 
 describe('HiveInfo', () => {
+
+    it('renders the hive name correctly', () => {
+        const { getByText } = render(<HiveInfo item={mockHiveItem} isDetailedView={false} />);
+        expect(getByText('Hive-test')).toBeTruthy();
+    })
+
     it('renders sensor data when isDetailedView is true', () => {
         const { getByText } = render(<HiveInfo item={mockHiveItem} isDetailedView={true} />);
-
         expect(getByText('Weight:')).toBeTruthy();
         expect(getByText('Hive Temp:')).toBeTruthy();
         expect(getByText('Humidity:')).toBeTruthy();
@@ -22,5 +27,14 @@ describe('HiveInfo', () => {
         expect(queryByText('Hive Temp:')).toBeNull();
         expect(queryByText('Humidity:')).toBeNull();
         expect(queryByText('Counter:')).toBeNull();
-    })
+    });
+
+    it('renders only Temperature, Wind, and Rain when isDetailedView is false', () => {
+        const { queryByText } = render(<HiveInfo item={mockHiveItem} isDetailedView={false} />);
+        expect(queryByText('21 °C')).not.toBeNull();
+        expect(queryByText('4 km/h')).not.toBeNull();
+        expect(queryByText('0.0mm 0%')).not.toBeNull();
+        expect(queryByText('Weight:')).toBeNull();
+    });
+    
 });
