dd22f8227e2948590cc82843ad242ecfcbb0d728
Sammish <samd93@hotmail.co.uk>
2024-03-11 13:24:30 +0100

    refactored delete hive dialogue to be responsive and match colour scheme

--- components/modals/DialogDeleteHive.tsx
+++ components/modals/DialogDeleteHive.tsx
@@ -15,14 +15,10 @@ const DialogDeleteHive = ({
   hideDialog,
   deleteHive,
 }: DialogDeleteHiveProps) => {
-  if (Platform.OS === "android" || Platform.OS === "ios") {
-    return <MobileModal hideDialog={hideDialog} deleteHive={deleteHive} />;
-  } else {
-    return <WebModal hideDialog={hideDialog} deleteHive={deleteHive} />;
-  }
+  return <DialogModal hideDialog={hideDialog} deleteHive={deleteHive} />;
 };
 
-const MobileModal = ({ hideDialog, deleteHive }: DialogDeleteHiveProps) => {
+const DialogModal = ({ hideDialog, deleteHive }: DialogDeleteHiveProps) => {
   const dynamicStyles = getStyles();
   const { userViewModel } = useContext(MobXProviderContext);
   const theme = useTheme();
@@ -30,17 +26,19 @@ const MobileModal = ({ hideDialog, deleteHive }: DialogDeleteHiveProps) => {
   return (
     <Portal>
       <Dialog
-        style={dynamicStyles.dialogStyleMobile}
+        style={{ ...dynamicStyles.overlayContainer, alignSelf: "center" }}
         visible={true}
         onDismiss={hideDialog}
       >
         <Dialog.Icon icon="alert" />
-        <Dialog.Title style={dynamicStyles.titleDialogGDPR}>
+        <Dialog.Title style={{ textAlign: "center" }}>
           {userViewModel.i18n.t("delete hive")}
         </Dialog.Title>
-        <Dialog.ScrollArea>
-          <ScrollView contentContainerStyle={dynamicStyles.scrollViewContent}>
-            <Text style={dynamicStyles.listItem}>
+        <Dialog.ScrollArea
+          style={{ borderBlockColor: theme.colors.primaryContainer }}
+        >
+          <ScrollView>
+            <Text style={theme.fonts.bodyMedium}>
               {userViewModel.i18n.t(
                 "do you wish to delete this hive permanently"
               )}
@@ -56,54 +54,8 @@ const MobileModal = ({ hideDialog, deleteHive }: DialogDeleteHiveProps) => {
             {userViewModel.i18n.t("cancel")}
           </Button>
           <Button
-            onPress={deleteHive}
-            accessibilityLabel="Delete hive"
-            accessibilityHint="Delete to delete the hive permanently"
-          >
-            {userViewModel.i18n.t("delete")}
-          </Button>
-        </Dialog.Actions>
-      </Dialog>
-    </Portal>
-  );
-};
-
-const WebModal = ({ hideDialog, deleteHive }: DialogDeleteHiveProps) => {
-  const dynamicStyles = getStyles();
-  const { userViewModel } = useContext(MobXProviderContext);
-  const theme = useTheme();
-
-  return (
-    <Portal>
-      <Dialog
-        style={dynamicStyles.dialogStyle}
-        visible={true}
-        onDismiss={hideDialog}
-      >
-        <Dialog.Icon icon="alert" />
-        <Dialog.Title style={dynamicStyles.titleDialogGDPR}>
-          {userViewModel.i18n.t("delete hive")}
-        </Dialog.Title>
-        <Dialog.ScrollArea>
-          <ScrollView
-            contentContainerStyle={{ paddingHorizontal: 24, paddingTop: 16 }}
-          >
-            <Text style={dynamicStyles.listItem}>
-              {userViewModel.i18n.t(
-                "do you wish to delete this hive permanently"
-              )}
-            </Text>
-          </ScrollView>
-        </Dialog.ScrollArea>
-        <Dialog.Actions>
-          <Button
-            onPress={hideDialog}
-            accessibilityLabel="Cancel the action"
-            accessibilityHint="Close if you don't wish to delete the hive"
-          >
-            {userViewModel.i18n.t("cancel")}
-          </Button>
-          <Button
+            style={{ backgroundColor: theme.colors.error }}
+            textColor={theme.colors.onError}
             onPress={deleteHive}
             accessibilityLabel="Delete hive"
             accessibilityHint="Delete to delete the hive permanently"
