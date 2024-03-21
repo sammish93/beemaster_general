1db9817fed1887cf82dd61e6505e2916d2727d92
stian96 <stiankru@hiof.no>
2024-03-21 12:01:48 +0100

    fixed code formatting

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     "plugin:import/typescript",
     "google",
     "plugin:@typescript-eslint/recommended",
-    "prettier"
+    "prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
@@ -21,15 +21,11 @@ module.exports = {
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
   ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-    "prettier"
-  ],
+  plugins: ["@typescript-eslint", "import", "prettier"],
   rules: {
-    "quotes": ["error", "double"],
+    quotes: ["error", "double"],
     "import/no-unresolved": 0,
-    "indent": ["error", 2],
-    "prettier/prettier": "error"
+    indent: ["error", 2],
+    "prettier/prettier": "error",
   },
 };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,18 @@
-import {onRequest} from "firebase-functions/v2/https";
+import { onRequest } from "firebase-functions/v2/https";
 import * as admin from "firebase-admin";
 
 admin.initializeApp();
 
 /**
  * Firebase Cloud Function for registering sensor weight data.
- * 
- * The function is exclusively designed to handle POST requests to add new weight 
+ *
+ * The function is exclusively designed to handle POST requests to add new weight
  * readings from sensors to our Firestore database. It expects 'userId', 'hiveId'
- * and 'weight' fields in the request body. 
- * 
+ * and 'weight' fields in the request body.
+ *
  * The function adds the weight data to the 'weightReading' subcollection under the
  * specific user´s hive document.
- * 
+ *
  * POST Request body format:
  * {
  *  "userId": "user´s unique identifier",
@@ -21,7 +21,6 @@ admin.initializeApp();
  * }
  */
 export const addWeightData = onRequest(async (request, response) => {
-
   if (request.method !== "POST") {
     response.status(405).send("Only 'POST' method allowed!");
     return;
@@ -32,7 +31,9 @@ export const addWeightData = onRequest(async (request, response) => {
   const weightData = request.body.weight;
 
   if (!userId || !hiveId || !weightData) {
-    response.status(400).send("Missing fields in request - userId, hiveId, weight.");
+    response
+      .status(400)
+      .send("Missing fields in request - userId, hiveId, weight.");
     return;
   }
 
@@ -42,20 +43,22 @@ export const addWeightData = onRequest(async (request, response) => {
   const timeStamp = date.toISOString();
 
   try {
-    
     // Construct path for new weight reading document.
-    const weightReadingsRef = admin.firestore()
-      .collection("users").doc(userId)
-      .collection("hives").doc(hiveId)
+    const weightReadingsRef = admin
+      .firestore()
+      .collection("users")
+      .doc(userId)
+      .collection("hives")
+      .doc(hiveId)
       .collection("weightReading");
 
     // Add the new weight reading with a Firestore-generated ID in the subcollection.
     const docRef = await weightReadingsRef.add({
       weight: weightData,
-      date: timeStamp
+      date: timeStamp,
     });
 
-    response.json({result: `Weight data added with ID: ${docRef.id}`});
+    response.json({ result: `Weight data added with ID: ${docRef.id}` });
   } catch (error) {
     console.error("Error writing document: ", error);
     response.status(500).send("Error writing document to db");
