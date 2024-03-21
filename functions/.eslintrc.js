cc935d026078fa35cd5e7701e0aaf049bc28868e
stian96 <stiankru@hiof.no>
2024-03-21 11:58:51 +0100

    added eslint and prettier to functions project

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     "plugin:import/typescript",
     "google",
     "plugin:@typescript-eslint/recommended",
+    "prettier"
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
@@ -23,10 +24,12 @@ module.exports = {
   plugins: [
     "@typescript-eslint",
     "import",
+    "prettier"
   ],
   rules: {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
     "indent": ["error", 2],
+    "prettier/prettier": "error"
   },
 };
