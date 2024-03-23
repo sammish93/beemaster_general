1f9e63922c6722ac99edd8f4dcf878f26880b685
stian96 <stiankru@hiof.no>
2024-03-23 13:14:55 +0100

    implemented skeleton of task definition

--- domain/tasks/notificationTask.ts
+++ domain/tasks/notificationTask.ts
@@ -2,4 +2,13 @@ import * as BackgroundFetch from 'expo-background-fetch';
 import * as TaskManager from 'expo-task-manager';
 
 // Unique identification name for the background task defined in TaskManager and BackgroundFetch.
-const BACKGROUND_FETCH_TASK = 'background-fetch-task';
+const BACKGROUND_TASK_NAME = 'background-fetch-task';
+
+// Definition of the task, parameters are the name we want to give the task and a function to be executed.
+TaskManager.defineTask(BACKGROUND_TASK_NAME, async () => {
+
+    // TODO: Implement the background task here.
+
+    // Return a result to indicate completion.
+    return BackgroundFetch.BackgroundFetchResult.NewData;
+});
