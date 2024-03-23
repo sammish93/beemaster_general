f2fba855dd524579f9d2e7f22f668b51c076d5ef
stian96 <stiankru@hiof.no>
2024-03-23 13:29:35 +0100

    added registration for the background task.

--- domain/tasks/notificationTask.ts
+++ domain/tasks/notificationTask.ts
@@ -1,10 +1,10 @@
 import * as BackgroundFetch from 'expo-background-fetch';
 import * as TaskManager from 'expo-task-manager';
 
-// Unique identification name for the background task defined in TaskManager and BackgroundFetch.
+// Unique identification for the background task defined in TaskManager and BackgroundFetch.
 const BACKGROUND_TASK_NAME = 'background-fetch-task';
 
-// Definition of the task, parameters are the name we want to give the task and a function to be executed.
+// Definition of the task, params are the name we want to give the task and a function to be executed.
 TaskManager.defineTask(BACKGROUND_TASK_NAME, async () => {
 
     // TODO: Implement the background task here.
@@ -12,3 +12,23 @@ TaskManager.defineTask(BACKGROUND_TASK_NAME, async () => {
     // Return a result to indicate completion.
     return BackgroundFetch.BackgroundFetchResult.NewData;
 });
+
+export const registerBackgroundTask = async () => {
+
+    // Check status for background updates.
+    const status = await BackgroundFetch.getStatusAsync();
+    switch (status) {
+        case BackgroundFetch.BackgroundFetchStatus.Restricted:
+        case BackgroundFetch.BackgroundFetchStatus.Denied:
+            console.log("BackgroundTask: Background update is disabled.");
+            return;
+        default: {
+            console.log("Register background task.");
+            await BackgroundFetch.registerTaskAsync(BACKGROUND_TASK_NAME, {
+                minimumInterval: 60 * 60, // 1 hour.
+                stopOnTerminate: false, // For android, continue running after the app exits.
+                startOnBoot: true, // For android, start after the device reboots.
+            });
+        }
+    }
+}
