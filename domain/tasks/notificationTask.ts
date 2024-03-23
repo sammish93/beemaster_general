d9b7fe59b69433b71b8c2e8295fd0f64948395fc
stian96 <stiankru@hiof.no>
2024-03-23 13:05:56 +0100

    added unique id for background task

---
+++ domain/tasks/notificationTask.ts
@@ -0,0 +1,5 @@
+import * as BackgroundFetch from 'expo-background-fetch';
+import * as TaskManager from 'expo-task-manager';
+
+// Unique identification name for the background task defined in TaskManager and BackgroundFetch.
+const BACKGROUND_FETCH_TASK = 'background-fetch-task';
