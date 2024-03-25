6bc54e8404924afcda690266204d21965d97049d
stian96 <stiankru@hiof.no>
2024-03-25 17:04:40 +0100

    .

--- functions/src/notification_task.ts
+++ functions/src/notification_task.ts
@@ -1,30 +1,24 @@
-import { onSchedule } from 'firebase-functions/v2/scheduler';
-import { logger } from 'firebase-functions';
-import * as admin from 'firebase-admin';
-import { getAllUsers } from './utils/utils';
+import { onSchedule } from "firebase-functions/v2/scheduler";
+import { logger } from "firebase-functions";
+import * as admin from "firebase-admin";
 
-admin.initializeApp();
+export const recurringBackgroundTask = onSchedule("every 60 min", async () => {
+  logger.log("Recurring background task started.");
 
-export const recurringBackgroundTask = onSchedule('every 60 min', async () => {
-    logger.log('Recurring background task started.');
+  const users = admin.firestore().collection("users");
+  const userSnapshot = await users.get();
+  if (userSnapshot.empty) {
+    logger.log("No active users found.");
+    return;
+  }
 
-    const userSnapshot = await getAllUsers(admin, 'users');
-    if (userSnapshot.empty) {
-        logger.log('No active users found.');
-        return;
-    }
+  // Retrieve userId.
+  for (const user of userSnapshot.docs) {
+    const userId = user.id;
+    logger.log(`Retrieved userId: ${userId} from firestore database.`);
 
-    // Retrieve userId.
-    for (const user of userSnapshot.docs) {
-
-        const userId = user.id;
-        logger.log(`Retrieved userId: ${userId} from firestore database.`);
-
-        // TODO: Retrieve preferences.
-        const userPreferences = user.data().notificationPreferences;
-        logger.log(`User preferences: ${userPreferences}`);
-    }
-
+    // TODO: Retrieve preferences.
+    const userPreferences = user.data().notificationPreferences;
+    logger.log(`User preferences: ${userPreferences}`);
+  }
 });
-
-
--- functions/src/utils/utils.ts
+++
@@ -1,5 +0,0 @@
-// Helper function to retrieve all active users.
-export const getAllUsers = async (admin: any, collectionName: string) => {
-    const users = admin.firestore().collection(collectionName);
-    return await users.get();
-} 
