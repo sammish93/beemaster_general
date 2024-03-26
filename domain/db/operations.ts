360599e7564577f6fed386205ecb58d9f01e975d
stian96 <stiankru@hiof.no>
2024-03-26 20:17:27 +0100

    fixed typo

--- domain/db/operations.ts
+++ domain/db/operations.ts
@@ -1,6 +1,5 @@
 import { auth, db } from '@/firebaseConfig';
 import { collection, getDocs } from 'firebase/firestore';
-import { User } from '@/models/user';
 
 /**
  * Retrieves all users from 'users' collection in Firestore.
@@ -11,6 +10,18 @@ export const getAllUsers = async (): Promise<Array<object>> => {
     const usersRef = collection(db, 'users');
     const users = await getDocs(usersRef);
     return users.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+}
+
+/**
+ * Retrieves all hives for a specific user from Firestore.
+ * 
+ * @param {User} userId - The user object containing the user's ID.
+ * @returns {Promise<Array<object>>} A list of hive objects.
+ */
+export const getUserHives = async (userId: string): Promise<Array<object>> => {
+    const hivesRef = collection(db, `users/${userId}/hives`);
+    const hives = await getDocs(hivesRef);
+    return hives.docs.map(doc => ({ id: doc.id, ...doc.data()}));
 }
 
 interface NotificationPreference {
