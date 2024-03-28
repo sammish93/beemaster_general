817536017ca39363bc97e563b3056e0fd83ebf84
stian96 <stiankru@hiof.no>
2024-03-28 14:24:44 +0100

    .

---
+++ domain/notification/notificationLogic.ts
@@ -0,0 +1,10 @@
+interface NotificationPreference {
+    email: boolean,
+    mobile: boolean,
+    sms: boolean
+}
+
+export const getActivatedPreferences = (preferences: NotificationPreference) => {
+    const activated = Object.entries(preferences).filter(([key, value]) => value === true);
+    return Object.fromEntries(activated);
+}
