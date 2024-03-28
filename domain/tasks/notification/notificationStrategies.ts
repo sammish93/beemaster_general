115e92873f73821559b2345022b6d475bbd9c386
stian96 <stiankru@hiof.no>
2024-03-28 18:37:08 +0100

    implemented weather notification

--- domain/tasks/notification/notificationStrategies.ts
+++ domain/tasks/notification/notificationStrategies.ts
@@ -1,16 +1,19 @@
+import forecast from "@/app/hive/forecast";
 import { areTemperaturesConsistentlyWarm, isSnowForecast, isWarmerEachDayInSpring } from "@/domain/notificationFunctions";
 import { CurrentForecast, DailyForecast, WeeklySimpleForecast } from "@/models/forecast";
 import { Hive } from "@/models/hive";
 import { User } from "@/models/user";
 
+interface WeatherData {
+    currentForecast: CurrentForecast,
+    dailyForecast: DailyForecast,
+    weeklyForecast: WeeklySimpleForecast
+}
+
 interface Props {
     user: User,
     hive: Hive,
-    weatherData: {
-        currentForecast: CurrentForecast,
-        dailyForecast: DailyForecast,
-        weeklyForecast: WeeklySimpleForecast
-    }
+    weatherData: WeatherData
 }
 
 export const notificationStrategies = {
@@ -44,10 +47,49 @@ export const notificationStrategies = {
     },
 
     weather: ({ user, hive, weatherData}: Props) => {
-        logMessage('weather', user, hive);
+        const weeklyTemperatures = getWeeklyTemperatureData(weatherData.weeklyForecast);
+        if (areTemperaturesConsistentlyWarm(weeklyTemperatures, weeklyTemperatures.length)) {
+            logMessage('warm trend', user, hive);
+        }
+        
+        const weatherConditions = getWeatherConditions(weatherData.weeklyForecast);
+        if (isSnowForecast(weatherConditions)) {
+            logMessage('snow forecast', user, hive);
+        }
+
+        if (isWarmerEachDayInSpring(weeklyTemperatures)) {
+            logMessage('warming trend in spring', user, hive);
+        }
+       
     }
 }
 
 const logMessage = (notificationType: string, user: User, hive: Hive) => {
     console.log(`Sending '${notificationType}' notification to ${user.email} for hive ${hive.id}`);
+}
+
+// Helper function.
+const getWeeklyTemperatureData = (weeklyForecast: WeeklySimpleForecast) => {
+    return [
+        weeklyForecast.currentForecast.temperature,
+        weeklyForecast.dayTwoForecast.temperature,
+        weeklyForecast.dayThreeForecast.temperature,
+        weeklyForecast.dayFourForecast.temperature,
+        weeklyForecast.dayFiveForecast.temperature,
+        weeklyForecast.daySixForecast.temperature,
+        weeklyForecast.daySevenForecast.temperature,
+    ];
+}
+
+// Helper function.
+const getWeatherConditions = (weeklyForecast: WeeklySimpleForecast) => {
+    return [
+        weeklyForecast.currentForecast.weatherType,
+        weeklyForecast.dayTwoForecast.weatherType,
+        weeklyForecast.dayThreeForecast.weatherType,
+        weeklyForecast.dayFourForecast.weatherType,
+        weeklyForecast.dayFiveForecast.weatherType,
+        weeklyForecast.daySixForecast.weatherType,
+        weeklyForecast.daySevenForecast.weatherType,
+    ];
 }
