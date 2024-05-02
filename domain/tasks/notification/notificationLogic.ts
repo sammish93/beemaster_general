d3d8336b203c1b66753073db33b0794554472fb5
Ashti Mawloudi <111912492+a5h71@users.noreply.github.com>
2024-05-02 11:42:56 +0200

    reading and writing notifications to db

--- domain/tasks/notification/notificationLogic.ts
+++ domain/tasks/notification/notificationLogic.ts
@@ -1,54 +1,65 @@
-import { Hive } from "@/models/hive";
-import { User } from "@/models/user";
-import { fetchWeatherForHive } from "../weather/weatherDataFetch";
-import { processWeatherDataForHive } from "../weather/weatherDataProcessor";
-import { notificationStrategies } from "./notificationStrategies"; 
+import { Hive } from "@/models/hive"
+import { User } from "@/models/user"
+import { fetchWeatherForHive } from "../weather/weatherDataFetch"
+import { processWeatherDataForHive } from "../weather/weatherDataProcessor"
+import { notificationStrategies } from "./notificationStrategies"
 
 interface NotificationPreference {
-    email: boolean,
-    mobile: boolean,
-    sms: boolean
+  email: boolean
+  mobile: boolean
+  sms: boolean
 }
 
-export const getActivatedPreferences = (preferences: NotificationPreference) => {
-    const activated = Object.entries(preferences).filter(([key, value]) => value === true);
-    return Object.fromEntries(activated);
+export const getActivatedPreferences = (
+  preferences: NotificationPreference
+) => {
+  const activated = Object.entries(preferences).filter(
+    ([key, value]) => value === true
+  )
+  return Object.fromEntries(activated)
 }
 
-export const evaluateAndSendNotification = async (user: User, hives: Hive[]) => {
+export const evaluateAndSendNotification = async (
+  user: User,
+  hives: Hive[]
+) => {
+  // Temporary solution - the other hives in the db is missing lots of fields.
+  const filteredHive = hives.filter(
+    (hive) => hive.id === "JJZf0Cc71rq1kMH4zGGT"
+  )
+  for (const hive of filteredHive) {
+    try {
+      // Get weather data for hive and process it.
+      const weatherData = await fetchWeatherForHive(hive)
+      const processedData = await processWeatherDataForHive(weatherData)
 
-    // Temporary solution - the other hives in the db is missing lots of fields.
-    const filteredHive = hives.filter(hive => hive.id === 'Es2njxWBdXky6zhu9UBZ');
-    for (const hive of filteredHive) {
-        try {
-            // Get weather data for hive and process it.
-            const weatherData = await fetchWeatherForHive(hive);
-            const processedData = await processWeatherDataForHive(weatherData);
-            
-            // Get user and hive preferences.
-            const userPreference = user.notificationTypePreference;
-            const hivePreference = hive.notificationTypePreference
+      // Get user and hive preferences.
+      const userPreference = user.notificationTypePreferences
+      const hivePreference = hive.notificationTypePreference
 
-            // Iterate over the strategies.
-            Object.keys(notificationStrategies).forEach(strategy => {
-                const notificationType = strategy as keyof typeof hive.notificationTypePreference;
-                const userPref = userPreference[notificationType];
-                const hivePref = hivePreference[notificationType];
-            
-                // Check the user and hive preferences.
-                if (userPref && hivePref) {
+      // Iterate over the strategies.
+      Object.keys(notificationStrategies).forEach((strategy) => {
+        const notificationType =
+          strategy as keyof typeof hive.notificationTypePreference
+        const userPref = userPreference[notificationType]
+        const hivePref = hivePreference[notificationType]
 
-                    const params = { user, hive, weatherData: processedData }
-                    
-                    // Execute strategy.
-                    notificationStrategies[notificationType](params);
-                }
-                else {
-                    console.log(`Notification ${notificationType} is turned off for both user and hive`);
-                }
-            });
-        } catch (error) {
-            console.error(`Error in processing hive ${hive.id} for user ${user.email}: ${error}`);
+        // Check the user and hive preferences.
+        if (userPref && hivePref) {
+          const params = { user, hive, weatherData: processedData }
+
+          // Execute strategy.
+          notificationStrategies[notificationType](params)
+        } else {
+          console.log(
+            `Notification ${notificationType} is turned off for both user and hive`
+          )
         }
-    };
+      })
+    } catch (error) {
+      console.error(
+        `Error in processing hive ${hive.id} for user ${user.email}: ${error}`
+      )
+    }
+  }
 }
--- viewModels/NotificationViewModel.ts
+++ viewModels/NotificationViewModel.ts
@@ -1,22 +1,19 @@
-import { action, makeAutoObservable, observable, runInAction } from "mobx";
-import * as Localization from "expo-localization";
-import { I18n } from "i18n-js";
-import en from "@/constants/localisation/en.json";
-import no from "@/constants/localisation/no.json";
+import { action, makeAutoObservable, observable, runInAction } from "mobx"
+import * as Localization from "expo-localization"
+import { I18n } from "i18n-js"
+import en from "@/constants/localisation/en.json"
+import no from "@/constants/localisation/no.json"
 import {
   PrecipitationMeasurement,
   TemperatureMeasurement,
   WeightMeasurement,
   WindSpeedMeasurement,
-} from "@/constants/Measurements";
-import {
-  availableLanguages,
-  availableCountries,
-} from "@/constants/LocaleEnums";
-import { Platform } from "react-native";
-import { notifications } from "@/data/notificationData";
-import { HiveNotification } from "@/models/notification";
-import { auth, db } from "@/firebaseConfig";
+} from "@/constants/Measurements"
+import { availableLanguages, availableCountries } from "@/constants/LocaleEnums"
+import { Platform } from "react-native"
+import { notifications } from "@/data/notificationData"
+import { HiveNotification } from "@/models/notification"
+import { auth, db } from "@/firebaseConfig"
 import {
   getDocs,
   collection,
@@ -26,59 +23,60 @@ import {
   updateDoc,
   deleteDoc,
   addDoc,
-} from "firebase/firestore";
+  Timestamp,
+} from "firebase/firestore"
 
 class NotificationViewModel {
-  notifications: HiveNotification[] = [];
+  notifications: HiveNotification[] = []
 
   constructor() {
-    makeAutoObservable(this);
+    makeAutoObservable(this)
   }
 
   @action async fetchNotifications() {
-    console.log("Fetching notifications for user");
-    const userId = auth.currentUser?.uid;
+    console.log("Fetching notifications for user")
+    const userId = auth.currentUser?.uid
     if (!userId) {
-      console.error("No user ID found, user might not be logged in");
-      return;
+      console.error("No user ID found, user might not be logged in")
+      return
     }
-    const notificationsRef = collection(db, `users/${userId}/notifications`);
-    const unreadQuery = query(notificationsRef, where("isRead", "==", false));
+    const notificationsRef = collection(db, `users/${userId}/notifications`)
+    const unreadQuery = query(notificationsRef, where("isRead", "==", false))
 
     try {
-      const querySnapshot = await getDocs(unreadQuery);
+      const querySnapshot = await getDocs(unreadQuery)
       if (querySnapshot.empty) {
-        console.log("No unread notifications found");
+        console.log("No unread notifications found")
       }
       runInAction(() => {
         this.notifications = querySnapshot.docs.map((doc) => {
-          const data = doc.data();
+          const data = doc.data()
           return {
             id: doc.id,
             hiveId: data.hiveId,
             notificationType: data.notificationType,
             message: data.message,
             isRead: data.isRead,
-            timestamp: new Date(data.timeStamp.seconds * 1000),
-          } as HiveNotification;
-        });
-        console.log("Loaded notifications: ", this.notifications);
-      });
+            timestamp: new Date(data.timestamp.seconds * 1000),
+          } as HiveNotification
+        })
+        console.log("Loaded notifications: ", this.notifications)
+      })
     } catch (error) {
-      console.error("Error fetching notifications:", error);
+      console.error("Error fetching notifications:", error)
     }
   }
 
   @action setNotifications(notifications: HiveNotification[]) {
-    console.log("Setting notifications", notifications);
-    this.notifications = notifications;
+    console.log("Setting notifications", notifications)
+    this.notifications = notifications
   }
 
   @action async addNotification(notification: HiveNotification) {
-    this.notifications = [...this.notifications, notification];
-    const userId = auth.currentUser?.uid;
+    this.notifications = [...this.notifications, notification]
+    const userId = auth.currentUser?.uid
     if (userId) {
-      const notificationsRef = collection(db, `users/${userId}/notifications`);
+      const notificationsRef = collection(db, `users/${userId}/notifications`)
 
       try {
         // Add the notification document to the Firestore collection
@@ -87,12 +85,12 @@ class NotificationViewModel {
           notificationType: notification.notificationType,
           message: notification.message,
           isRead: notification.isRead,
-          timestamp: notification.timestamp,
-        });
+          timestamp: Timestamp.fromDate(new Date(notification.timestamp)),
+        })
 
-        console.log("Notification added to Firestore successfully");
+        console.log("Notification added to Firestore successfully")
       } catch (error) {
-        console.error("Error adding notification to Firestore:", error);
+        console.error("Error adding notification to Firestore:", error)
       }
     }
   }
@@ -100,80 +98,80 @@ class NotificationViewModel {
   @action getNotificationFromId(
     notificationId: string
   ): HiveNotification | undefined {
-    return this.notifications.find((item) => item.id === notificationId);
+    return this.notifications.find((item) => item.id === notificationId)
   }
 
   @action async modifyNotification(notification: HiveNotification) {
     const noteIndex = this.notifications.findIndex(
       (notificationObject) => notificationObject.id === notification.id
-    );
+    )
 
     if (noteIndex !== -1) {
-      this.notifications[noteIndex] = notification;
-      const userId = auth.currentUser?.uid;
+      this.notifications[noteIndex] = notification
+      const userId = auth.currentUser?.uid
 
       if (userId) {
         const notificationRef = doc(
           db,
           `users/${userId}/notifications/${notification.id}`
-        );
+        )
 
         try {
           await updateDoc(notificationRef, {
             isRead: notification.isRead,
-          });
-          console.log("Notification updated successfully", notification.id);
+          })
+          console.log("Notification updated successfully", notification.id)
 
           // Check if isRead is true, then delete the notification
           if (notification.isRead) {
-            await deleteDoc(notificationRef);
-            console.log("Notification deleted successfully", notification.id);
+            await deleteDoc(notificationRef)
+            console.log("Notification deleted successfully", notification.id)
           }
         } catch (error) {
-          console.error("Error updating notification in Firestore:", error);
+          console.error("Error updating notification in Firestore:", error)
         }
       }
     }
   }
 
   @action async markNotificationsAsRead() {
-    const userId = auth.currentUser?.uid;
+    const userId = auth.currentUser?.uid
     if (userId) {
       this.notifications.forEach(async (notification) => {
-        notification.isRead = true;
+        notification.isRead = true
 
         const notificationRef = doc(
           db,
           `users/${userId}/notifications/${notification.id}`
-        );
+        )
 
         try {
           await updateDoc(notificationRef, {
             isRead: true,
-          });
+          })
         } catch (error) {
           console.error(
             "Error marking notification as read in Firestore:",
             error
-          );
+          )
         }
-      });
+      })
     }
   }
 
   @action getUnreadNotificationAmount() {
     const unreadNotificationsAmount = this.notifications.filter(
       (notification) => !notification.isRead
-    ).length;
+    ).length
 
-    return unreadNotificationsAmount;
+    return unreadNotificationsAmount
   }
 
   @action removeNotification(notificationId: string) {
     this.notifications = this.notifications.filter(
       (notificationObject) => notificationObject.id !== notificationId
-    );
+    )
   }
 }
 
-export default new NotificationViewModel();
+export default new NotificationViewModel()
