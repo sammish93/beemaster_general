bb327c7461c0f4f6c5d99e4382f690d12586aa94
stian96 <stiankru@hiof.no>
2024-05-02 14:34:43 +0200

    fixed typo

--- models/hiveModel.ts
+++ models/hiveModel.ts
@@ -1,24 +1,21 @@
-import {
-  NotificationTypePreference,
-  NotificationType,
-} from "@/constants/Notifications";
-import { HiveNote } from "./note";
+import { NotificationTypePreference } from "@/constants/Notifications"
+import { HiveNote } from "./note"
 
 export interface HiveModel {
-  id: string;
-  name: string;
-  filters: string[];
-  latLng: { lat?: number; lng?: number };
-  temperature?: number;
-  weight?: number;
-  humidity?: number;
-  beeCount?: number;
-  notes: HiveNote[];
-  preferences: NotificationTypePreference;
-  queen?: QueenBee;
+    id: string
+    name: string
+    filters: string[],
+    latLng: { lat?: number, lng?: number },
+    temperature?: number
+    weight?: number
+    humidity?: number
+    beeCount?: number
+    notes: HiveNote[]
+    preferences: NotificationTypePreference
+    queen?: QueenBee
 }
 
 export interface QueenBee {
-  id: string;
-  dateOfBirth: Date;
+    id: string,
+    dateOfBirth: Date
 }
